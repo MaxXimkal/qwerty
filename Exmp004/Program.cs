6bae73c0449d2ce1318b318838ad52cbed9139d9
Maksim <maksim_shobanov@mail.ru>
2022-10-03 21:45:37 +0300

    Особое приветствие

---
+++ Exmp004/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "максим")
+{
+    Console.Write("Здравствуйте, " + username + "!");
+}
+else
+{
+    Console.Write("Привет, " + username + ".");
+}
