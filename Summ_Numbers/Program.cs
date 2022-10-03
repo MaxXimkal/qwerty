fe1afc1d80165157f4e93a0b46ec94f6ad6eb866
Maksim <maksim_shobanov@mail.ru>
2022-10-03 20:42:38 +0300

    Сумма и частное двух чисел

---
+++ Summ_Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите два числа ");
+string a_str = Console.ReadLine();
+double a = Convert.ToDouble(a_str);
+string b_str = Console.ReadLine();
+double b = Convert.ToDouble(b_str);
+int summ = Convert.ToInt32(a + b);
+double del = a/b;
+Console.WriteLine("Сумма чисел " + a + " и " + b + " равна " + summ);
+Console.Write("Частное чисел " + a + " и " + b + " равно " + del);
